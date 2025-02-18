dfa4e3a21a6da5444039fe577abf5f71a6d13479
makardyomin <makardyomin1@gmail.com>
2025-02-18 14:44:14 +0300

    test

--- src/test/java/com/example/petproject/PetProjectApplicationTests.java
+++ src/test/java/com/example/petproject/PetProjectApplicationTests.java
@@ -1,11 +1,21 @@
 package com.example.petproject;
-
+import com.example.petproject.service.ProjectService;
+import com.example.petproject.model.Project;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class PetProjectApplicationTests {
 
+    @Autowired
+    private ProjectService projectService;
 
-
+    @Test
+    void testGetProjectById() {
+        final long A1 = 0;
+        Project project = projectService.getProjectById(A1);
+        assertNotNull(project);
+    }
 }
